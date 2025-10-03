650a33ff699f7b665c058bb9dcfce169a10cb551
Lucas dias <lucasdias26@hotmail.com.br>
2025-10-03 12:56:53 -0300

    implementando login

--- app.py
+++ app.py
@@ -1,36 +1,10 @@
-from flask import Flask, request, render_template, jsonify, redirect, url_for, session, flash
-import bcrypt
+from flask import Flask
 from config import ConfigFlask
 
 app = Flask(__name__)
 ConfigFlask.configApp(app)
 
-usuarios = {
-    "teste@email.com": bcrypt.hashpw("123456".encode("utf-8"), bcrypt.gensalt())
-}
 
-@app.route("/")
-def index():
-    return render_template("login.html")
 
-@app.route("/login", methods=["POST"])
-def login():
-    email = request.form.get("email")
-    senha = request.form.get("password").encode("utf-8")
-
-    if email in usuarios and bcrypt.checkpw(senha, usuarios[email]):
-        session["usuario"] = email
-        flash("Login realizado com sucesso!", "success")
-        return redirect(url_for("dashboard"))
-    else:
-        flash("Email ou senha inválidos", "danger")
-        return redirect(url_for("index"))
-
-@app.route("/dashboard")
-def dashboard():
-    if "usuario" not in session:
-        return redirect(url_for("index"))
-    return f"Bem-vindo, {session['usuario']}!"
-    
 if __name__ == "__main__":
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+from src.routes import login
 
 
 class ConfigFlask:
     def __init__(self):
         pass
-
+    
     @staticmethod
     def configApp(app):
         app.secret_key = " "
+        app.register_blueprint(login.loging_bp)
         
